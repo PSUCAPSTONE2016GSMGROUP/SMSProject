df9551df8b80e21338fa8a96b6f22d2efec34e23
thenamelessprogrammer <habib9780@gmail.com>
2016-12-07 16:25:24 -0800

    Update GPRS_SMSread2.0.ino

--- GPRS_SMSread2.0.ino
+++ GPRS_SMSread2.0.ino
@@ -1,20 +1,3 @@
-/*
-GPRS SMS Read
-
-This sketch is used to test seeeduino GPRS_Shield's reading SMS 
-function.To make it work, you should insert SIM card
-to Seeeduino GPRS Shield,enjoy it!
-
-There are two methods to read SMS:
-  1. GPRS_LoopHandle.ino  -> in order to recieve "+CMTI: "SM"" 
-      may be you need to send this command to your shield: "AT+CNMI=2,2,0,0,0"
-  2. GPRS_SMSread.ino -> you have to check if there are any 
-      UNREAD sms, and you don't need to check serial data continuosly
-
-create on 2015/05/14, version: 1.0
-by op2op2op2(op2op2op2@hotmail.com)
-*/
-
 #include <GPRS_Shield_Arduino.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
@@ -30,13 +13,17 @@ by op2op2op2(op2op2op2@hotmail.com)
 char message[MESSAGE_LENGTH];
 int messageIndex = 0;
 
+// create variables to hold data
 char phone[16];
 char datetime[24];
 
-GPRS gprsTest(PIN_TX,PIN_RX,BAUDRATE);//RX,TX,PWR,BaudRate
+GPRS gprsTest(PIN_TX,PIN_RX,BAUDRATE); //RX,TX,PWR,BaudRate
 
 void setup() {
+  // begin com at BAUDRATE of 9600 
   Serial.begin(9600);
+  
+  // keep trying to initialize until initialization is successful
   while(!gprsTest.init()) {
       Serial.print("init error\r\n");
       delay(1000);
@@ -44,7 +31,7 @@ void setup() {
   delay(3000);  
   Serial.println("Init Success, please send SMS message to me!");
   
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin A0, and A3 as outputs.
   pinMode(A0, OUTPUT);
   pinMode(A3, OUTPUT);
 }
@@ -56,8 +43,10 @@ void loop() {
       gprsTest.readSMS(messageIndex, message, MESSAGE_LENGTH, phone, datetime);           
       //In order not to full SIM Memory, is better to delete it
       gprsTest.deleteSMS(messageIndex);
-
+      
+     // check if the message was sent by an authorized phone number 
       if (strcmp (phone,"5038101652") == 0 || strcmp (phone,"9713403666") == 0 || strcmp (phone,"6199226759") == 0 || strcmp (phone,"5039603745") == 0) {
+        // apply appropriate action based on the message 
         if(String(message).equalsIgnoreCase("Unlock")){
           digitalWrite(A0, HIGH); // turn the LED on by making the voltage high
           digitalWrite(A3, HIGH);
@@ -74,9 +63,9 @@ void loop() {
         }
       }
       else {
-          gprsTest.sendSMS(phone,MESSAGE2); 
+          gprsTest.sendSMS(phone,MESSAGE2); // send unauthorized user to the unauthorized sender 
       }
-
+      
       Serial.print("From number: ");
         Serial.println(phone);  
         Serial.print("Datetime: ");
